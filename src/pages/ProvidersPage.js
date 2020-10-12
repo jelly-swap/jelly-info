9fc6519f0bbdc9ea50ed1c7a0f1f287a8a52ec4f
AndonMitev <mitevandon94@gmail.com>
2020-10-12 09:54:10 +0300

    Successfully get all providers

--- src/pages/ProvidersPage.js
+++ src/pages/ProvidersPage.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { withRouter } from 'react-router-dom'
+import axios from 'axios'
 
 function ProvidersPage() {
+  const [providersData, setProvidersData] = useState('');
+
+  useEffect(() => {
+    (async() => {
+      try {
+        const response = await axios.get('https://jelly-jam.herokuapp.com/api/v1/info/get');
+
+        console.log(response.data)
+
+        setProvidersData(response.data);
+      } catch (error) {
+        console.log('Error getting liquidity providers', error);
+      }
+     
+    })();
+  }, [])
+
   return (
     <div></div>
   )
